aae5409ebc5554cfea0b64cb516a4aa6bb9d1576
Terax235 <terax235@gmail.com>
2018-08-20 18:22:47 +0200

    fixed package.json version, edited meta contents and added version to html

--- build-docs.js
+++ build-docs.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const package = require('./package.json');
 
 let methods = [];
 
@@ -15,20 +16,20 @@ var basecode = `<html>
 <head>
 <meta charset="utf8">
 <title>splat2api</title>
-<meta name="description" content="A public Splatoon 2 API." />
+<meta name="description" content="Splat2API is a public Splatoon 2 API, which you can use to retrieve information on current maps." />
 <!-- Twitter Card data -->
 <meta name="twitter:card" content="summary">
 <meta name="twitter:site" content="@Terax235">
-<meta name="twitter:title" content="Splatoon 2 API">
-<meta name="twitter:description" content="A public Splatoon 2 API.">
+<meta name="twitter:title" content="Splat2API">
+<meta name="twitter:description" content="Splat2API is a public Splatoon 2 API, which you can use to retrieve information on current maps.">
 <meta name="twitter:creator" content="@Terax235">
 
 <!-- Open Graph data -->
-<meta property="og:title" content="Splatoon 2 API" />
+<meta property="og:title" content="Splat2API" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="http://api.splatoon.terax235.me/" />
-<meta property="og:description" content="A public Splatoon 2 API." />
-<meta property="og:site_name" content="terax235" />
+<meta property="og:description" content="Splat2API is a public Splatoon 2 API, which you can use to retrieve information on current maps." />
+<meta property="og:site_name" content="Terax235" />
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <style>
@@ -43,7 +44,7 @@ body {
 <body>
 <h1>Welcome to splat2api</h1>
 This API shows information about map schedules in Splatoon 2.<br>
-By <a href="https://github.com/Terax235" target="_blank">Terax235</a> - <a href="https://github.com/Terax235/splat2api" target="_blank">Source Code</a>
+By <a href="https://github.com/Terax235" target="_blank">Terax235</a> - Version ${package.version} - <a href="https://github.com/Terax235/splat2api" target="_blank">Source Code</a>
 <br><br>
 <h2>Documentation</h2>
 <table style="width:100%">
