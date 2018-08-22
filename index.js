fe6d7c6b6cd4547438cd5740c94c0496bd443a97
Terax235 <terax235@gmail.com>
2018-08-22 22:40:05 +0200

    changed config properties and some other code

--- index.js
+++ index.js
@@ -12,46 +12,49 @@ const morgan = require("morgan");
 app.use(morgan("dev"));
 app.set("json spaces", 4);
 
-let global = {};
-
 process.title = "SplatAPI";
 
+async function updateStats() {
+    config.endpoints = { map_schedules: await SplatAPI.getSchedules(), coop_schedules: await SplatAPI.getCoopSchedules(), stages: await SplatAPI.getStages(), merchandises: await SplatAPI.getMerch(), festivals: { active: await SplatAPI.getFestivals("active"), pasts: await SplatAPI.getFestivals("pasts") } };
+    config.endpoints.current_map_schedule = SplatAPI.currentMatches(config.endpoints.map_schedules);
+    config.endpoints.current_coop_schedule = SplatAPI.currentCoop(config.endpoints.coop_schedules);
+    return 'ok';
+};
+
 app.listen(config.port, async () => {
+    require('child_process').exec("node build-docs.js"); // Updating documentation every time the script is started.
     console.log("Listening on port " + config.port + ".");
-    config.Schedules = await SplatAPI.getSchedules();
-    config.Current = SplatAPI.currentMatches(config.Schedules);
-    config.Coops = await SplatAPI.getCoopSchedules();
-    config.Coop_Current = SplatAPI.currentCoop(config.Coops);
-    config.Stages = await SplatAPI.getStages();
+    await updateStats();
     let newex = false;
     let juststarted = true;
     let routes = fs.readdirSync("./routes/");
+    if (!routes[0]) {
+        console.log("No routes detected. Quitting process...");
+        process.exit(0);
+    };
     console.log("Endpoints:");
     for (let file of routes) {
         var f = require("./routes/" + file);
-        for (var use of f) {
-            if (!use.method) { use.method = "get" }
-            app[use.method.toLowerCase()](use.name, use.export);
-            console.log("  " + use.name + " (" + use.method.toUpperCase() + ")");
-        }
-    }
+        if (f[0]) {
+            for (var use of f) {
+                if (!use.method) { use.method = "get" }
+                app[use.method.toLowerCase()](use.name, use.export);
+                console.log("  " + use.name + " (" + use.method.toUpperCase() + ")");
+            };
+        };
+    };
     setInterval(async () => {
         const MapSets = await Database.Maps.findOne();
         if (!MapSets) { newex = true; await Database.Maps.create({ latest: 100 }); }
         const Maps = await Database.Maps.findOne({ where: { latest: { [require("sequelize").Op.lte] : Date.now() }}});
         if (!Maps && juststarted) { juststarted = false }
         if (!Maps) return;
-        let newtime = new Date(moment(config.Current.regular.end_time*1000).add(2, "hours")).getTime();
-        if (newex) { newtime = config.Current.regular.end_time*1000; newex = false }
-        if (juststarted) { newtime = config.Current.regular.end_time*1000; juststarted = false }
+        let newtime = new Date(moment(config.endpoints.current_map_schedule.regular.end_time*1000).add(2, "hours")).getTime();
+        if (newex) { newtime = config.endpoints.current_map_schedule.regular.end_time*1000; newex = false }
+        if (juststarted) { newtime = config.endpoints.current_map_schedule.regular.end_time*1000 }
         await Database.Maps.update({ latest: newtime }, { where: { id: Maps.id }});
-        await console.log(moment().format("LLL") + " > Updating information...");
-        config.Schedules = await SplatAPI.getSchedules();
-        config.Current = await SplatAPI.currentMatches(config.Schedules);
-        config.Coops = await SplatAPI.getCoopSchedules();
-        config.Coop_Current = SplatAPI.currentCoop(config.Coops);
-        config.Stages = await SplatAPI.getStages();
+        if (juststarted) { juststarted = false; return }
+        console.log(moment().format("LLL") + " > Updating information...");
+        await updateStats();
     }, 1000);
 });
-
-module.exports = global;
